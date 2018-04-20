87089091ade07a2115235cbb822ec6b654f14182
Bryan Ayala <ayala.bryan9@gmail.com>
2018-04-20 13:13:33 -0500

    its a test

--- app/src/main/java/com/example/ayala/irosster/RosterActivity.java
+++ app/src/main/java/com/example/ayala/irosster/RosterActivity.java
@@ -9,5 +9,6 @@ public class RosterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roster);
+        int number = 0;
     }
 }
