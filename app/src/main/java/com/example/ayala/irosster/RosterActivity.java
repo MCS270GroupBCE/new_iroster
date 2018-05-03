2f7eb35b5d0627bb00f29023737dc25cbdb6396b
Bryan Ayala <ayala.bryan9@gmail.com>
2018-05-03 13:18:03 -0500

    edited layout from the crime projects

--- app/src/main/java/com/example/ayala/irosster/RosterActivity.java
+++ app/src/main/java/com/example/ayala/irosster/RosterActivity.java
@@ -9,6 +9,5 @@ public class RosterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roster);
-        int number = 0;
     }
 }
---
+++ app/src/main/java/com/example/ayala/irosster/TeamClass.java
@@ -0,0 +1,28 @@
+package com.example.ayala.irosster;
+
+import java.util.UUID;
+
+/**
+ * Created by ayala on 5/3/2018.
+ */
+
+public class TeamClass {
+
+    private UUID mId;
+    private String mTeamName;
+
+    public TeamClass(){
+        mId = UUID.randomUUID();
+
+    }
+
+    public UUID getmId(){
+        return mId;
+    }
+
+    public String getmTeamName(){
+        return mTeamName;
+    }
+
+}
+
